e07da9eab4c508b5ecefa9d85dee6fbc302ca489
Leonid <bohdanovleonid93@gmail.com>
2025-11-06 08:22:34 +0200

    add def index and def detail in app.py

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+
+@app.route("/")
+def index():
+    books = Book.query.all()
+    return render_template("index.html", books=books)
+
+
+@app.route("/book/<int:id>")
+def detail(id):
+    book = Book.query.get_or_404(id)
+    return render_template("detail.html", book=book)
